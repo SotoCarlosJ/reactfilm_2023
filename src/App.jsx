6633f80a0eaea089e62fe53a8bb735093435c447
Carlos Soto <carloss.empire@gmail.com>
2023-11-06 11:19:35 -0300

    [cfg] - actualizando el readme

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ const App = () => {
   return (
     <div>
       <h1>App para Pelis</h1>
-<<<<<<< HEAD
       <h2>Esto es para que el cliente pruebe</h2>
-=======
-      <h2>Esto es QA</h2>
->>>>>>> QA
     </div>
   )
 }
